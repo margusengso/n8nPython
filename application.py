13ffc3a20d3ebe64c549918e0462add5ded37244
margusengso <margusengso@hotmail.com>
2024-10-28 08:04:47 +0200

    init socket

--- application.py
+++ application.py
@@ -1,10 +1,26 @@
+import os
 from flask import Flask, jsonify
+from flask_socketio import SocketIO, send
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
+app.config['WS_SECRET_KEY'] = os.getenv('WS_SECRET_KEY')
+
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({'message': 'Hello n8n project'})
 
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Message received: {msg}")
+    reply = f"Server says: You sent '{msg}'"
+    send(reply)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))  # Retrieve port from environment variables, default to 5000
+    socketio.run(app, debug=True, host='0.0.0.0', port=port)
